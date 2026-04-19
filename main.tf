eca697a0e1a96e6f00b3f66459847c03a0833e97
Bjørnar Aassveen <bjornar@aassveen.com>
2026-04-19 16:01:53 +0200

    Temporarily disable access packages due to permissions - focus on PIM eligibility

--- main.tf
+++ main.tf
@@ -37,47 +37,47 @@ resource "azuread_directory_role_eligibility_schedule_request" "pim_eligibility"
 resource "time_static" "start" {}
 
 # Create access package catalog
-resource "azuread_access_package_catalog" "pim_catalog" {
-  display_name = "PIM Access Packages"
-  description  = "Catalog for PIM access packages to directory roles"
-}
+# resource "azuread_access_package_catalog" "pim_catalog" {
+#   display_name = "PIM Access Packages"
+#   description  = "Catalog for PIM access packages to directory roles"
+# }
 
 # Associate each group as a resource in the catalog
-resource "azuread_access_package_resource_catalog_association" "group_associations" {
-  for_each = azuread_group.pim_groups
+# resource "azuread_access_package_resource_catalog_association" "group_associations" {
+#   for_each = azuread_group.pim_groups
 
-  catalog_id             = azuread_access_package_catalog.pim_catalog.id
-  resource_origin_id     = each.value.object_id
-  resource_origin_system = "AadGroup"
-}
+#   catalog_id             = azuread_access_package_catalog.pim_catalog.id
+#   resource_origin_id     = each.value.object_id
+#   resource_origin_system = "AadGroup"
+# }
 
 # Create access package for each group
-resource "azuread_access_package" "pim_access_packages" {
-  for_each = azuread_group.pim_groups
+# resource "azuread_access_package" "pim_access_packages" {
+#   for_each = azuread_group.pim_groups
 
-  catalog_id   = azuread_access_package_catalog.pim_catalog.id
-  display_name = "PIM ${each.key} Access Package"
-  description  = "Self-service access to PIM group for ${each.key} role"
-}
+#   catalog_id   = azuread_access_package_catalog.pim_catalog.id
+#   display_name = "PIM ${each.key} Access Package"
+#   description  = "Self-service access to PIM group for ${each.key} role"
+# }
 
 # Link each group resource to its access package
-resource "azuread_access_package_resource_package_association" "group_access" {
-  for_each = azuread_access_package.pim_access_packages
+# resource "azuread_access_package_resource_package_association" "group_access" {
+#   for_each = azuread_access_package.pim_access_packages
 
-  access_package_id               = each.value.id
-  catalog_resource_association_id = azuread_access_package_resource_catalog_association.group_associations[each.key].id
-  access_type                     = "Member"
-}
+#   access_package_id               = each.value.id
+#   catalog_resource_association_id = azuread_access_package_resource_catalog_association.group_associations[each.key].id
+#   access_type                     = "Member"
+# }
 
 # Create self-service assignment policy for each access package
-resource "azuread_access_package_assignment_policy" "self_service_policies" {
-  for_each = azuread_access_package.pim_access_packages
+# resource "azuread_access_package_assignment_policy" "self_service_policies" {
+#   for_each = azuread_access_package.pim_access_packages
 
-  access_package_id = each.value.id
-  display_name      = "Self-service PIM ${each.key} Access"
-  description       = "Self-service policy for requesting access to ${each.key} PIM group"
+#   access_package_id = each.value.id
+#   display_name      = "Self-service PIM ${each.key} Access"
+#   description       = "Self-service policy for requesting access to ${each.key} PIM group"
 
-  requestor_settings {
-    scope_type = "AllExistingDirectoryMemberUsers"
-  }
-}
+#   requestor_settings {
+#     scope_type = "AllExistingDirectoryMemberUsers"
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "pim_group_ids" {
   value       = { for k, v in azuread_group.pim_groups : k => v.object_id }
 }
 
-output "access_package_ids" {
-  description = "IDs of the created access packages"
-  value       = { for k, v in azuread_access_package.pim_access_packages : k => v.id }
-}
+# output "access_package_ids" {
+#   description = "IDs of the created access packages"
+#   value       = { for k, v in azuread_access_package.pim_access_packages : k => v.id }
+# }
 
-output "catalog_id" {
-  description = "ID of the access package catalog"
-  value       = azuread_access_package_catalog.pim_catalog.id
-}
+# output "catalog_id" {
+#   description = "ID of the access package catalog"
+#   value       = azuread_access_package_catalog.pim_catalog.id
+# }
